463a79792fc0e2681fe45596204864fb481b1824
ganeshkondaka <kondakaganesh2003@outlook.com>
2024-09-17 21:44:01 +0530

    added tweets

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,8 +1,15 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
 
-export default  function Homepage() {
+interface tweettype {
+    id: number,
+    username: string,
+    tweet: string
+}
+
+export default function Homepage() {
     const [loggedin_user, setloggedin_user] = useState("")
+    const [tweets, settweets] = useState<tweettype[]>([])
 
 
     const fetchdata = async () => {
@@ -12,22 +19,36 @@ export default  function Homepage() {
                 throw new Error("token not found");
             }
             const headers = {
-                Authorization: token, 
+                Authorization: token,
             };
             // const response = await axios.get("http://localhost:3000/api/prod/alltweets",{headers});
             const response = await axios.get("http://localhost:3000/api/prod/alltweets");
-            console.log(response.data.data)
-            
+            settweets(response.data.data)
+            console.log(tweets)
 
         } catch (error) {
             console.error('Error fetching chat data:', error);
         }
     }
-    useEffect(()=>{
+
+    useEffect(() => {
         fetchdata()
-    })
+    }, [])
+
     return (
         <div>
+            <span></span>
+            <span>
+                <div></div>
+                <div></div>
+                {tweets.map((atweet) => (
+                    <div key={atweet.id} className="p-4 border-b border-gray-300">
+                        <div className="font-bold">{atweet.username}</div>
+                        <div>{atweet.tweet}</div>
+                    </div>
+                ))}
+
+            </span>
 
         </div>
     )
